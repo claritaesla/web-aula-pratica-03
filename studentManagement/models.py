e49117609d8943014c44a6f5688f32496929181c
claritaesla <clara.lourenco@academico.ifpb.edu.br>
2023-11-14 11:28:01 -0300

    alter

--- studentManagement/models.py
+++ studentManagement/models.py
@@ -7,3 +7,8 @@ class Student(models.Model):
     Classe responsável pelo cadastro de alunos
     """
     name = models.CharField(max_length=50, blank=False)
+    matricula = models.CharField(max_length=20, blank=False, default="0")
+    idade= models.CharField(max_length=2, blank=False, default='0')
+    email= models.EmailField(max_length=254, blank=False, default='example@email.com')
+    telefone= models.CharField(max_length=14, blank=False, default='+55 12 345678900')
+    
